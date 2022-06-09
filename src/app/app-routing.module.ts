bff05f24d7b178353ad4891d051b005a49d9393f
Sarah <sarah.eiger@thought-train.co>
2022-06-09 20:47:13 +0100

    Instantiating app rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CanLoadGuard } from './modules/core/guards/can-load.guard';
+import { PlaylistResolver } from './modules/core/resolvers/playlist.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./modules/homepage/homepage.module').then(m => m.HomepageModule)
+  },
+
+  {
+    path: 'playlists',
+    loadChildren: () =>
+      import('./modules/playlists/playlists.module').then(m => m.PlaylistsModule),
+    resolve: { playlistData: PlaylistResolver }
+  },
+
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./modules/account/account.module').then(m => m.AccountModule),
+    canLoad: [CanLoadGuard]
+  },
+
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/login/login.module').then(m => m.LoginModule),
+  },
+
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
