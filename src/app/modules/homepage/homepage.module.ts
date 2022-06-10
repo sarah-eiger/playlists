be0ac38e6b7d05289ec9b00e764a8c8fbb0a0611
Sarah <sarah.eiger@thought-train.co>
2022-06-10 13:16:27 +0100

    Create homepage

--- src/app/modules/homepage/homepage.module.ts
+++ src/app/modules/homepage/homepage.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './component/homepage.component';
+import { CoreModule } from '../core/core.module';
 
+const homepageRoutes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  }
+];
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomepageComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild(homepageRoutes),
+    CoreModule
+  ],
 })
 export class HomepageModule { }
