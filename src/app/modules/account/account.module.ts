ec00240cd575f0d593cc2fa7fb4e9712b592c219
Sarah <sarah.eiger@thought-train.co>
2022-06-10 13:18:44 +0100

    Update account module

--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/component/account.component';
 
-
+const accountRoutes: Routes = [
+  {
+    path: '',
+    component: AccountComponent,
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [AccountComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    RouterModule.forChild(accountRoutes)
+  ],
+  exports: [RouterModule]
 })
 export class AccountModule { }
