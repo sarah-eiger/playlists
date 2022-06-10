3651f347d41f669f8ac4e4c155740fe17ceb229c
Sarah <sarah.eiger@thought-train.co>
2022-06-10 13:19:03 +0100

    Implementation of the canLoad guard

--- src/app/modules/core/guards/can-load.guard.ts
+++ src/app/modules/core/guards/can-load.guard.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import {LoginService} from '../../login/services/login.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CanLoadGuard implements CanLoad {
+
+  constructor(private loginService: LoginService, private router: Router) { }
+
   canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    throw new Error('Method not implemented.');
+    const authToken = this.loginService.getAuthCookie();
+    if (!!authToken) {
+      console.log('can load');
+      return true;
+    } else {
+      this.loginService.logout();
+      this.router.navigate(['login']);
+      console.log('cannot load');
+      return false;
+    }
   }
+  
   // canActivate(
   //   route: ActivatedRouteSnapshot,
   //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
