5d4caafa0f93553a36ccda3fc6b69ab7e3757608
Sarah <sarah.eiger@thought-train.co>
2022-06-10 14:58:12 +0100

    Adding routing animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { LoadingService } from './modules/core/services/loading.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('fadeAnimation', [
+      transition('* => *', [
+          style({ opacity: 0 }),
+          animate(1000, style({ opacity: 1 }))
+      ]),
+  ])]
 })
 
 export class AppComponent {
@@ -13,5 +22,9 @@ export class AppComponent {
   constructor(public loadingService: LoadingService) { }
 
   ngOnInit() {}
+
+  public prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet?.activatedRouteData;
+  }
 
 }
